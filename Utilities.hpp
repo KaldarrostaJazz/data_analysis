fd9b8e778744bb61695593ed12f0adea9d95768c
Giuseppe Sguera <giuseppe.sguera2@studio.unibo.it>
2021-06-23 11:10:36 +0200

    Utilities

---
+++ Utilities.hpp
@@ -0,0 +1,40 @@
+#ifndef UTILITIES_HPP
+#define UTILITIES_HPP
+
+#include <vector>
+#include <stdexcept>
+#include <cassert>
+#include <cmath>
+//////////////////////////////////////////////////
+// Class declarations ////////////////////////////
+//////////////////////////////////////////////////
+class Fit {
+	private:
+		std::vector<double> observed;
+	public:
+		Fit(std::vector<double>& data)
+			: observed{data} {
+				if (observed.empty())
+					throw std::runtime_error("The data vector is empty!\n");
+			}
+		double Chi_Square(std::vector<double>& expected);
+		double RChi_Square(std::vector<double>& expected, int ndof);
+};
+//////////////////////////////////////////////////
+// Methods definitions ///////////////////////////
+//////////////////////////////////////////////////
+inline double Fit::Chi_Square(std::vector<double>& expected) {
+	assert(observed.size() == expected.size());
+	int N = observed.size();
+	double chi = 0.;
+	for (int i = 0; i != N; ++i)
+		chi += std::pow(observed[i] - expected[i], 2) / expected[i];
+	assert(chi >= 0.);
+	return chi;
+}
+
+inline double Fit::RChi_Square(std::vector<double>& expected, int ndof) {
+	double chi = Chi_Square(expected);
+	return chi / ndof;
+}
+#endif
